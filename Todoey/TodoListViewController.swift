96fdce694741f97dcb30d3314e1a173e1cd2c951
David Erosa <soulnet@gmail.com>
2018-10-28 18:32:19 +0100

    Persist user data with NSDefaults

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -9,14 +9,24 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-
+    let SAVED_ITEMS_KEY = "TodoArrayList"
+    
     //override var prefersStatusBarHidden: Bool { return true }
     
-    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let savedItems = defaults.array(forKey: SAVED_ITEMS_KEY) as? [String] {
+            itemArray = savedItems
+        }
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        print("didReceiveMemoryWarning")
     }
     
     //MARK - Tableview Datasource Methods
@@ -39,13 +49,41 @@ class TodoListViewController: UITableViewController {
         } else {
             cell?.accessoryType = .checkmark
         }
-    
+        
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    //MARK - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add Item", message: "Enter your new item", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            // What will happen one the user press the add item button
+            print("Action OK")
+            
+            if let newItemText = textField.text{
+                if newItemText.count > 0 {
+                    self.itemArray.append(newItemText)
+                    self.defaults.set(self.itemArray, forKey: self.SAVED_ITEMS_KEY)
+                    self.tableView.reloadData()
+                    print("Saved!")
+                }
+            }
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Add your item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
     }
     
     
     
-
-
 }
 
